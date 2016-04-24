d1cd32166d9fba2427a845b8ed5031b4af11b763
arikkrol@gmail.com <arikkrol@gmail.com>
2016-04-24 14:15:03 +0300

    remark added

--- translation/translationEngine.go
+++ translation/translationEngine.go
@@ -6,7 +6,6 @@ import(
     "github.com/arikkrol/asciiTranslator/translation/io"
 )
 
-
 type translationEngine struct{
     numberProvider io.IInputProvider 
     outputHandler io.IOutputHandler
@@ -18,6 +17,10 @@ func NewTranslationEngine(inFile string, outFile string) *translationEngine{
     te := &translationEngine{}
     te.numberProvider = io.NewAsciiNumberProvider(inFile)
     te.outputHandler = io.NewInvoiceNumberOutputHandler(outFile)
+    
+    
+    //TODO: bootstrap translation dic
+    
     return te;
 }
 
