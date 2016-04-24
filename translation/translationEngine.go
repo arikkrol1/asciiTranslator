798c938d286f6b798ab152f706decde0e9610e55
arikkrol@gmail.com <arikkrol@gmail.com>
2016-04-24 14:37:19 +0300

    translation dic bootstraping added

--- translation/translationEngine.go
+++ translation/translationEngine.go
@@ -17,13 +17,25 @@ func NewTranslationEngine(inFile string, outFile string) *translationEngine{
     te := &translationEngine{}
     te.numberProvider = io.NewAsciiNumberProvider(inFile)
     te.outputHandler = io.NewInvoiceNumberOutputHandler(outFile)
-    
-    
-    //TODO: bootstrap translation dic
-    
+    te.initTranslationDic()
     return te;
 }
 
 func (te translationEngine) Translate(){
     //TODO: complete
+}
+
+func (te translationEngine) initTranslationDic(){
+    te.translationDic = make(map[string]string)
+    
+    te.translationDic[" _ | ||_|"] = "0";
+    te.translationDic["     |  |"] = "1";
+    te.translationDic[" _  _||_ "] = "2";
+    te.translationDic[" _  _| _|"] = "3";
+    te.translationDic["   |_|  |"] = "4";
+    te.translationDic[" _ |_  _|"] = "5";
+    te.translationDic[" _ |_ |_|"] = "6";
+    te.translationDic[" _   |  |"] = "7";
+    te.translationDic[" _ |_||_|"] = "8";
+    te.translationDic[" _ |_| _|"] = "9";
 }
