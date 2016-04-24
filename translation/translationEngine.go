99828fd53f64967ae00ddaafe39ccac473cbfd1f
arikkrol@gmail.com <arikkrol@gmail.com>
2016-04-24 12:49:41 +0300

    translationEngine stub added

--- translation/translationEngine.go
+++ translation/translationEngine.go
@@ -7,9 +7,16 @@ import(
 )
 
 
-
-type TranslationEngine struct{
+type translationEngine struct{
     numberProvider *io.IInputProvider 
     outputHandler *io.IOutputHandler
+    translationDic map[string]string
 }
 
+//NewTranslationEngine ctor 
+func NewTranslationEngine(*io.IInputProvider, *io.IOutputHandler) *translationEngine{
+    
+    
+    //TODO: complete
+    return nil;
+}
